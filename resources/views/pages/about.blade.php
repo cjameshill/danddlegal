35831927933c4661fda12b3427a39b5b30415001
Chris Hill <hello@christopherjameshill.com>
2017-02-14 06:19:37 -0500

    Removed Disul from all context

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -8,9 +8,9 @@
             <div class="twelve columns">
                 <a href="#test"><header class="always-center-text"><span class="white">About D&amp;D Legal</span></header></a>
                 <p class="about-text column-text white">D&amp;D is a client focused full service law consultancy with broadly demonstrated expertise and experience in business law. Recently established early 2013, after noticing a gap in the UK market, D&amp;D is a dynamic boutique consultancy dedicated to bespoke solutions to both individual and corporate clients. With a Turkish speaking lawyer based in London, D&amp;D are experts in their respective fields, providing outstanding client service. D&amp;D also collaborate with sector specific law firms and notaries based in Turkey and the UK. D&amp;D will be key to your success in a dynamic, growing market.</p>
-                <p class="about-text column-text white">D&amp;D’s clientele includes multinational retail enterprises and individuals. Their primary purpose is to represent and advise clients in finding, providing and implementing clear and applicable solutions focusing on the specific needs of any given transaction, legal question or dispute that our clients are involved in. D&amp;D provide legal assistance throughout all foreign business or individual investments and all kind of legal matters.
+                <p class="about-text column-text white">D&amp;D’s clientele includes multinational retail enterprises and individuals. Our primary purpose is to represent and advise clients in finding, providing and implementing clear and applicable solutions focusing on the specific needs of any given transaction, legal question or dispute that our clients are involved in. D&amp;D provide legal assistance throughout all foreign business or individual investments and all kind of legal matters.
                 </p>
-                <p class="about-text column-text white">Both founding directors worked and studied at some of the most prestigious universities and law firms in the UK and Turkey. Their departure from major law firms came from a desire for a more personalized approach and closer relationships with their clients. D&amp;D communicate in both English and Turkish which contributes to their ability to provide the best possible client experience.
+                <p class="about-text column-text white">Our founder worked and studied at some of the most prestigious universities and law firms in the UK and Turkey. Starting her own law practice came from a desire for a more personalized approach and closer relationships with the clients. D&amp;D communicate in both English and Turkish which contributes to the ability to provide the best possible client experience.
                 </p>
 
             </div>
@@ -21,26 +21,12 @@
 
         <div class="row">
             <div class="twelve columns">
-                <header class="always-center-text"><span class="lightgrey">More about our founding partners</span></header>
+                <header class="always-center-text"><span class="lightgrey">More about the managing director</span></header>
 
 
             </div>
         </div>
 
-        <div class="row">
-            <div class="twelve columns">
-                <div class="panel transparent">
-                    <header class="text-left"><span class="dark">Dilsu Ekin Undar, LL.M.</span></header>
-                    <p class="about-text column-text dark">Dilsu completed her LL.B at the globally renowned Bilkent University which has a reputation developing lawyers who have the ability to understand and manage global legal issues. Following her graduation from law school, she completed her internship at a prominent law firm in Istanbul followed by her admission to Istanbul Bar Association. She started working at an esteemed law firm in Istanbul ranked amongst the most respected and successful law firms focusing on intellectual property law, commercial law and litigation.
-                    </p>
-                    <p class="about-text column-text dark">Her drive and passion in intellectual property law, meant she was soon appointed as the supervisor of Intellectual Property Law department and manager of the trademark and patent agency company, an affiliate of the law firm as regards intellectual property registrations and oppositions. She has practiced wide range of IP Law with a special emphasis on trademark protection, from trademark registration to vigorous litigation and enforcement of trademark, copyright and design matters. In 2011 she decided to take it a step further and was rewarded with an LLM degree on intellectual property law at Queen Mary, University of London.
-                    </p>
-                    <p class="about-text column-text dark">Dilsu and her business partner started D&amp;D to support the emergence of Turkey as one of the fastest developing and most strategic markets in the world and one with which the UK has strong and consistent business relationships. Dilsu and her business partner are able to offer legal consultancy services to both individuals and businesses due to their education and professional background in both jurisdictions.
-                    </p>
-                </div>
-            </div>
-
-        </div>
         <a name="test"></a>
         <div class="row">
             <div class="twelve columns">
@@ -67,14 +53,11 @@
         <div class="row">
             <div class="twelve columns">
                 <header class="always-center-text"><span class="white">Our Alliance in Istanbul</span></header>
-                <p class="about-text white">We have established a solution partnership with Istanbul based Umit Yayla Attorneys at Law so as to save time and effort on cross border matters. Turkish and English speaking lawyers at these offices will assist our local and foreign clients on legal needs in Turkey, primarily in Istanbul.
+                <p class="about-text white">D&amp;D Legal has established a solution partnership with Izmir based Tokgoz Law Firm so as to save time and effort on cross border matters. Turkish and English speaking lawyers at these offices assist local and foreign clients in any given transaction, legal dispute that D&amp;D’s clients are involved in Turkey.
                     <br> </p>
 
             </div>
         </div>
-
-    </section>
-
 
     </section>
 
